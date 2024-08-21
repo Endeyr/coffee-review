70d71f62922b7c7260b7e4c418114997dbeffa9c
Endeyr <aaron.thompson42594@gmail.com>
2024-08-20 20:50:51 -0400

    add context to root

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,32 @@
+import Navbar from "@/components/navbar";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+// Page layout
 const Root = () => {
+  const [username, setUsername] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [notificationMessage, setNotificationMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
   return (
     <section className="w-full h-screen">
-      <div className="flex items-center justify-center h-full">
-        <Outlet />
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-full gap-2">
+        <Outlet
+          context={{
+            username,
+            setUsername,
+            isLoggedIn,
+            setIsLoggedIn,
+            notificationMessage,
+            setNotificationMessage,
+            isLoading,
+            setIsLoading,
+            errorMessage,
+            setErrorMessage,
+          }}
+        />
       </div>
     </section>
   );
