a2e8ed6e83e332f8926f2c2a61255da712ade675
Endeyr <aaron.thompson42594@gmail.com>
2024-09-10 10:04:16 -0400

    move map provider to own page

---
+++ src/routes/google/MapPage.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/container";
+import MapProvider from "@/components/google/provider";
+
+const MapPage = () => {
+  return (
+    <Container>
+      <MapProvider />
+    </Container>
+  );
+};
+export default MapPage;
