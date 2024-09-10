72abe09b11779e39138d563509a40b9dba46e6a9
Endeyr <aaron.thompson42594@gmail.com>
2024-09-10 10:06:01 -0400

    add components to home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import MapProvider from "./components/google/provider";
+import CallToAction from "./components/home/callToAction";
+import Hero from "./components/home/hero";
+import Identity from "./components/home/identity";
+import Services from "./components/home/services";
+import Testimonials from "./components/home/testimonials";
 
 // Home Page
 function App() {
   return (
     <>
-      {/* Hero */}
-      {/* Services */}
-      {/* Identity */}
-      {/* Testimonials */}
-      {/* CallToAction */}
-      <MapProvider />
+      <Hero />
+      <Services />
+      <Identity />
+      <Testimonials />
+      <CallToAction />
     </>
   );
 }
