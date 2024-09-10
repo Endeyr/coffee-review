c83225445db2a3ec0b23544a6d9344bf2fe2a02c
Endeyr <aaron.thompson42594@gmail.com>
2024-09-10 09:18:57 -0400

    add shadcn form to call to action

---
+++ src/components/home/callToAction.tsx
@@ -0,0 +1,12 @@
+import Container from "../container";
+
+const CallToAction = () => {
+  return (
+    <Container>
+      <h2>Title</h2>
+      <p>Details</p>
+      {/* Contact Form shadcn */}
+    </Container>
+  );
+};
+export default CallToAction;
